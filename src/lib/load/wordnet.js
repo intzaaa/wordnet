22a8687c35d8089186bf98442ae859ac2f63cbbd
HYH! <huangyinhaow@outlook.com>
2023-10-01 01:32:32 +0800

    update

--- src/lib/load/wordnet.js
+++ src/lib/load/wordnet.js
@@ -1,9 +1,12 @@
+import * as fzstd from 'fzstd';
 import { readable } from 'svelte/store';
 
 async function get() {
-	const res = await fetch('/wordnet.json.gz');
+	const res = await fetch('/wordnet-json-zstd');
 	if (res.ok) {
-		return readable(await res.json());
+		return readable(
+			await new Response(fzstd.decompress(new Uint8Array(await res.arrayBuffer()))).json()
+		);
 	} else {
 		return readable(false);
 	}
